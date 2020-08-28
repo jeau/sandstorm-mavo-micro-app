f7a4329be1a097e815603a763c552a31b44f6858
Thierry Pasquier <thierry.pasquier@emf.ccsti.eu>
2020-08-28 17:22:58 +0200

    added medias and datas folders for sandstorm context

--- .sandstorm/launcher.sh
+++ .sandstorm/launcher.sh
@@ -1,13 +1,18 @@
 mkdir -p /var/microapp
 mkdir -p /var/microapp/pages
 mkdir -p /var/www
+mkdir -p /var/www/assets
+mkdir -p /var/www/audios
+mkdir -p /var/www/datas
 mkdir -p /var/www/images
+mkdir -p /var/www/videos
 
 if [ ! -L "/var/microapp/templates" ]; then
      ln -s /opt/app/templates /var/microapp/templates
 fi
 
 rsync --recursive --ignore-existing /opt/app/pages/ /var/microapp/pages/
+rsync --recursive --ignore-existing /opt/app/datas/ /var/microapp/datas/
 
 cd /var/microapp
 exec /opt/app/microapp --data /var/microapp
