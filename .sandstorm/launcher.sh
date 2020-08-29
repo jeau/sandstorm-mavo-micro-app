3c88adeb1eeed95b59e2f26799b645c0a0b8cce5
Thierry Pasquier <thierry.pasquier@emf.ccsti.eu>
2020-08-29 17:21:07 +0200

    cleanly upgrade app for existing grains

--- .sandstorm/launcher.sh
+++ .sandstorm/launcher.sh
@@ -1,18 +1,24 @@
 mkdir -p /var/microapp
 mkdir -p /var/microapp/pages
+mkdir -p /var/microapp/data
+mkdir -p /var/microapp/audios
+mkdir -p /var/microapp/images
+mkdir -p /var/microapp/videos
 mkdir -p /var/www
-mkdir -p /var/www/assets
-mkdir -p /var/www/audios
-mkdir -p /var/www/datas
-mkdir -p /var/www/images
-mkdir -p /var/www/videos
 
 if [ ! -L "/var/microapp/templates" ]; then
      ln -s /opt/app/templates /var/microapp/templates
 fi
 
+if [ ! -L "/var/microapp/assets" ]; then
+     ln -s /opt/app/assets /var/microapp/assets
+fi
+
 rsync --recursive --ignore-existing /opt/app/pages/ /var/microapp/pages/
-rsync --recursive --ignore-existing /opt/app/datas/ /var/microapp/datas/
+rsync --recursive --ignore-existing /opt/app/data/ /var/microapp/data/
+rsync --recursive --ignore-existing /opt/app/audios/ /var/microapp/audio/
+rsync --recursive --ignore-existing /opt/app/images/ /var/microapp/images/
+rsync --recursive --ignore-existing /opt/app/videos/ /var/microapp/videos/
 
 cd /var/microapp
 exec /opt/app/microapp --data /var/microapp
